d671a99c8c52b27da86b420bb68fe800a741043e
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-09-06 08:23:57 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <thread>
+using namespace std;
 
+void driverCar(string driverName) {
+  cout << driverName << "is Driving " << endl;
+  this_thread::sleep_for(chrono::seconds(2));
+  cout << driverName << "is done driving " << endl;
+}
+
+int main()
+{
+  thread t1(driverCar, "oggy");
+  thread t2(driverCar, "jack");
+
+  t1.join();
+  t2.join();
+  return 0;
+}
